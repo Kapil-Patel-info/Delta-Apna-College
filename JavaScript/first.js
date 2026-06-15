d005de5a741a5a2984793ed877af4ba3a1a39506
thekapilpatel <thekapilpatelofficial@gmail.com>
2026-06-15 14:36:32 +0530

    15 july

---
+++ JavaScript/first.js
@@ -0,0 +1,86 @@
+// let key = 2;
+
+// switch (key) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     case 4:
+//         console.log("Friday");
+//         break;
+
+//     default:
+//         console.log("Wrong Input");
+//         break;
+// }
+
+
+// let num = 5;
+//  let newnum = num++;
+// console.log(`before = ${newnum}`);
+
+// newnum = ++num;
+
+
+// console.log(`after = ${newnum}`);
+
+
+
+// let name = " ";
+
+// console.log(name.length);
+
+// // console.log(name[0]);
+
+
+// // console.log(name[9]);
+
+
+
+// let str = "kpppppil";
+
+// if(str.includes('a') && str.length > 3){
+//     console.log("a h bhai"); 
+// }else{
+//     console.log("bad h ");
+// }
+
+
+// let str = "akapil";
+
+// if(str[0]=='A' || str[0]=='a' &&   str.length > 5  ){
+//     console.log("sahi h ");
+// }else{
+//     console.log("BHAG");
+// }
+
+
+
+// let str = "apple,banana,mango";
+// console.log(str);
+// console.log(str.split(","));
+
+
+
+
+// let month =["january", "july", "march", "august"];
+
+// console.log(month.splice(0,2,"july","june"));
+
+// console.log(month)
+
+
+
+let lang = ["c","java","js","html","css"];
+
+
+ let newarr = lang.slice(2,1).reverse();
+console.log(newarr)
