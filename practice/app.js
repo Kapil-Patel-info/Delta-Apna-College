f8827c538fbb031505d047ce850b750604d32674
thekapilpatel <thekapilpatelofficial@gmail.com>
2026-06-20 08:00:16 +0530

    20 june

--- practice/app.js
+++ practice/app.js
@@ -1,87 +1,111 @@
-let p = document.createElement('p');
-let h3 = document.createElement('h3');
-let div = document.createElement('div');
-let divh1 = document.createElement('h1');
-let divp =  document.createElement('p');
-let hr = document.createElement('hr');
-let qinput = document.createElement('input');
-let qbtn = document.createElement('button')
+// let p = document.createElement('p');
+// let h3 = document.createElement('h3');
+// let div = document.createElement('div');
+// let divh1 = document.createElement('h1');
+// let divp =  document.createElement('p');
+// let hr = document.createElement('hr');
+// let qinput = document.createElement('input');
+// let qbtn = document.createElement('button')
 
 
+// // 
 
-
-let body = document.querySelector('body');
-document.querySelector('p');
-h3.querySelector('h3');
-div.querySelector('div');
-divh1.querySelector('h1');
-divp.querySelector('p');
-hr.querySelector('hr');
-qinput.querySelector('input');
-qbtn.querySelector('qbtn')
+// let body = document.querySelector('body');
+// document.querySelector('p');
+// h3.querySelector('h3');
+// div.querySelector('div');
+// divh1.querySelector('h1');
+// divp.querySelector('p');
+// hr.querySelector('hr');
+// qinput.querySelector('input');
+// qbtn.querySelector('qbtn')
 
    
-// -------------------------------
+// // -------------------------------
 
-p.classList.add('pera');
-p.innerText = "Hey iam Red!";
-body.append(p);
-p.style.color = "red";
-
-
-h3.innerText = "iam h3 blue ";
-h3.style.color = "blue"
-body.insertAdjacentElement("beforebegin",h3)
+// p.classList.add('pera');
+// p.innerText = "Hey iam Red!";
+// body.append(p);
+// p.style.color = "red";
 
 
-div.style.backgroundColor = "pink"
-div.innerText ="div hai"
-div.style.border="2px solid black";
-divh1.innerText="hello iam h1 inside  div ";
- divp.innerText="mee tooooo";
+// h3.innerText = "iam h3 blue ";
+// h3.style.color = "blue"
+// body.insertAdjacentElement("beforebegin",h3)
 
 
-
-
-body.insertAdjacentElement("beforebegin",div)
-div.append(divh1);
- div.append(divp);
-
-
-
- body.appendChild(hr)
-
-// -----------------------------------------------------------
-
-qinput.placeholder="enter your name";
-hr.appendChild(qinput);
-
-qbtn.innerText="click me !"
-qbtn.style.backgroundColor = "black";
-qbtn.style.color="white";
-
-body.appendChild(qbtn)
-
-qbtn.setAttribute('id',"mybtn");
-
-let taskbtn = document.getElementById('mybtn');
-taskbtn.style.backgroundColor = "blue";
-
-body.append(taskbtn)
+// div.style.backgroundColor = "pink"
+// div.innerText ="div hai"
+// div.style.border="2px solid black";
+// divh1.innerText="hello iam h1 inside  div ";
+//  divp.innerText="mee tooooo";
 
 
 
 
+// body.insertAdjacentElement("beforebegin",div)
+// div.append(divh1);
+//  div.append(divp);
 
-let h1Practice = document.createElement('h1');
-document.querySelector('h1Practice');
 
-h1Practice.innerText="helloooo h1 su";
 
-h1Practice.style.textDecoration = "underline";
-h1Practice.style.textDecorationColor = "red";
+//  body.appendChild(hr)
 
-body.append(h1Practice);
+// // -----------------------------------------------------------
+
+// qinput.placeholder="enter your name";
+// hr.appendChild(qinput);
+
+// qbtn.innerText="click me !"
+// qbtn.style.backgroundColor = "black";
+// qbtn.style.color="white";
+
+// body.appendChild(qbtn)
+
+// qbtn.setAttribute('id',"mybtn");
+
+// let taskbtn = document.getElementById('mybtn');
+// taskbtn.style.backgroundColor = "blue";
+
+// body.append(taskbtn)
+
+
+
+
+
+// let h1Practice = document.createElement('h1');
+// document.querySelector('h1Practice');
+
+// h1Practice.innerText="helloooo h1 su";
+
+// h1Practice.style.textDecoration = "underline";
+// h1Practice.style.textDecorationColor = "red";
+
+// body.append(h1Practice);
+
+
+
+
+// let btn = document.querySelector('button');
+
+// btn.addEventListener("dblclick", function () {
+//     console.log("you double clicked the btn !");
+// });
+
+
+// function sayhello(){
+// console.log("btn clicked ");
+// }
+
+// btn.onclick=sayhello;
+
+
+
+
+
+
+
+
 
 
 
